d4f510fbf2b440bfe6dc7ab96031018a0193bc46
PiSethSeam <7piseth@gmail.com>
2024-04-19 02:25:04 +0700

    Beautified game element

--- src/components/pages/Game-Store.jsx
+++ src/components/pages/Game-Store.jsx
@@ -1,7 +1,56 @@
 import React from "react";
-import { FaRegHeart, FaDownload, FaHeart } from "react-icons/fa";
-import { CiHeart } from "react-icons/ci";
-import got from "../../assets/games/Lies of P.jpg";
+import { FaRegHeart, FaDownload } from "react-icons/fa";
+import got from "../../assets/games/Ghost-of-Tsushima.jpg";
+import lop from "../../assets/games/Lies of P.jpg";
+import hzd from "../../assets/games/Horizon-Zero-Dawn.jpg";
+import hfw from "../../assets/games/Horizon-Forbidden-West.jpg";
+import spm from "../../assets/games/Marvels Spider Man Miles Morales.jpg";
+import gow from "../../assets/games/God of war.jpg";
+
+const gameInfo = [
+  {
+    id: 1,
+    cover: got,
+    title: "Ghost of Tsushima",
+    likeCount: 122,
+    downCount: 24,
+  },
+  {
+    id: 1,
+    cover: lop,
+    title: "Life of P",
+    likeCount: 102,
+    downCount: 76,
+  },
+  {
+    id: 1,
+    cover: hzd,
+    title: "Horizon Zero Dawn",
+    likeCount: 126,
+    downCount: 98,
+  },
+  {
+    id: 1,
+    cover: hfw,
+    title: "Horizon Forbidden West",
+    likeCount: 322,
+    downCount: 124,
+  },
+  {
+    id: 1,
+    cover: spm,
+    title: "Spider Man Miles Morales",
+    likeCount: 112,
+    downCount: 86,
+  },
+  {
+    id: 1,
+    cover: gow,
+    title: "God of war",
+    likeCount: 132,
+    downCount: 76,
+  },
+];
 
 const GameStore = () => {
   return (
@@ -10,26 +59,33 @@ const GameStore = () => {
       data-aos-offset="100"
       className="grid grid-cols-2 gap-2 md:grid-cols-4 w-full px-1 mt-20"
     >
-      <div
-        className="bg-white bg-opacity-10 dark:bg-black dark:bg-opacity-10 p-3 
-      rounded-lg flex flex-col items-center gap-1 relative"
-      >
-        <p className="absolute sm:top-36 top-32 bg-black font-bold text-xl p-4 rounded-lg bg-opacity-70 text-yellow-500">Life of P</p>
-        <img
-          src={got}
-          alt="html.png"
-          className="sm:h-56 h-48 object-contain rounded-lg"
-        />
-        <div className="flex items-center gap-2">
-          <FaRegHeart />
-          <p>124</p>
-          <FaDownload />
-          <p>67</p>
+      {gameInfo.map(({ id, cover, title, likeCount, downCount }) => (
+        <div
+          className="bg-white bg-opacity-10 dark:bg-black dark:bg-opacity-10 p-3 
+         rounded-lg flex flex-col items-center gap-1 relative"
+        >
+          <div className="relative">
+            <p className="absolute bottom-0 text-white font-bold text-lg text-center w-full bg-black pt-1 bg-opacity-30 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+              {title}
+            </p>
+            <img
+              src={cover}
+              alt="not found.png"
+              className="sm:h-56 h-48 object-contain rounded-lg"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <FaRegHeart />
+            <p>{likeCount}</p>
+            <FaDownload />
+            <p>{downCount}</p>
+          </div>
+          <button className="btn-primary text-white bg-opacity-10 rounded-2xl px-5 py-1">
+            Download
+          </button>
         </div>
-        <button className="btn-primary text-white bg-opacity-10 rounded-2xl px-5 py-1">
-          Download
-        </button>
-      </div>{" "}
+      ))}
     </div>
   );
 };

12b21d94f46842fc9797a9b19ef834150a84188f
PiSethSeam <7piseth@gmail.com>
2024-04-18 17:17:41 +0700

    Changed name in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
         </a>
       </div>
       <p class="md:mr-5 text-center">
-        Designed and Developed by <b>IT Phnom Den</b>
+        Designed and Developed by <b>អាយធី ភ្នំដិន</b>
       </p>
     </div>
   );
